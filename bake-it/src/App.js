d1cd906fbbb43519ce0d509aeafbfd98fef5d93c
Jess Merry <merryja.email@gmail.com>
2022-06-17 15:24:16 -0400

    initial set up create react app

---
+++ bake-it/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bake It 'Til You Make It
+      </header>
+    </div>
+  );
+}
+
+export default App;
