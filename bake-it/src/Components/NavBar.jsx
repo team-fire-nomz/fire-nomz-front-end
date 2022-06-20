c0d05a67d85cdac9faba0243a62018ef77a6f9e8
hjoyheath <hillaryjoyheath@gmail.com>
2022-06-20 10:51:29 -0400

    updates made

--- bake-it/src/Components/NavBar.jsx
+++ bake-it/src/Components/NavBar.jsx
@@ -5,8 +5,8 @@ import { AppBar, Toolbar, Typography, Stack, Button } from "@mui/material"
 
 const NavBar = ({ handleLogout, isLoggedIn }) => {
   return (
-    <AppBar position='static' gutterBottom>
-        <Toolbar gutterBottom>
+    <AppBar position='static'>
+        <Toolbar>
             <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
                 Bake It Till You Make It
             </Typography>
