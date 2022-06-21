42a02e5f1bb98e003a8ff4104186f9218497537e
hjoyheath <hillaryjoyheath@gmail.com>
2022-06-21 09:00:06 -0400

    navbar update with signin and signup, RecipeDetail routed

---
+++ bake-it/src/Components/Recipe.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import { Card } from "@mui/material";
+
+const Recipe = (props) => {
+
+    return (
+        <Card>
+        <div>
+        <Link to={`/recipe/${props.id}`}>{props.title}</Link>
+        <p>Chef: {props.chef}</p>
+        <h5>Baked on: {props.created_at}</h5>
+        </div>
+        </Card>
+    )
+}
+
+export default Recipe;
---
+++ bake-it/src/Components/RecipeList.jsx
@@ -0,0 +1,43 @@
+import Recipe from "./Recipe";
+import { Link, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function RecipeList(props) {
+  const [recipes, setRecipes] = useState([]);
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const requestUrl = `https://bake-it-till-you-make-it.herokuapp.com/api/recipes/?${searchParams}`;
+    console.log(requestUrl);
+    axios.get(requestUrl).then((res) => {
+      console.log(res);
+      setRecipes(res.data);
+    });
+  }, [searchParams]);
+
+  return (
+    <div>
+      {recipes.length > 0 ?
+        recipes.map((recipe) => (
+          <Recipe
+            id={recipe.id}
+            title={recipe.title}
+            ingredients={recipe.ingredients}
+            recipe={recipe.recipe}
+            chef={recipe.chef}
+            created_at={recipe.created_at}
+            key={recipe.id}
+          />
+        ))
+      :
+      <h3>No results found</h3>
+      }
+
+      <Link to="/addrecipe">ADD YOUR NEW RECIPE</Link>
+    </div>
+  );
+}
+
+export default RecipeList;
+
