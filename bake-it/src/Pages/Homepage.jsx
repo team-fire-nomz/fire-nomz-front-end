ba558d9a31a9701374821f2abe5f06c4ce39ac22
hjoyheath <hillaryjoyheath@gmail.com>
2022-06-29 21:36:28 -0400

    update to sign in showing only when user is logged out

--- bake-it/src/Pages/Homepage.jsx
+++ bake-it/src/Pages/Homepage.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import RecipeList from "../Components/RecipeList";
 import { Container, Box, Button } from "@mui/material";
 
+function Homepage(props) {
+  console.log(props);
 
-function Homepage (props){
-console.log(props)
-
-   return (
-      <Container sx={{overflow: 'scroll'}}>
-         <Box textAlign="center">
+  return (
+    <Container sx={{ overflow: "scroll" }}>
+      {!props.isLoggedIn && (
+        <>
+          <Box textAlign="center">
             <Button
-               size="large" 
-               variant="outlined" 
-               type="submit"
-               component={Link}
-               to= "/signin"
-               >Sign in
-            </Button>
-         </Box>
-         <Box textAlign="center">
-            <Button
-            component={Link}
-            to= "/signup"
-            size="small"
-            variant="contained"
+              size="large"
+              variant="outlined"
+              type="submit"
+              component={Link}
+              to="/signin"
             >
-            WANT TO JOIN OTHER BAKERS? PLEASE SIGN UP.
+              Sign in
             </Button>
-         </Box>
-         <RecipeList {...props} />
-      </Container>
-)}
+          </Box>
+          <Box textAlign="center">
+            <Button
+              component={Link}
+              to="/signup"
+              size="small"
+              variant="contained"
+            >
+              WANT TO JOIN OTHER BAKERS? PLEASE SIGN UP.
+            </Button>
+          </Box>
+        </>
+      )}
+      <RecipeList {...props} />
+    </Container>
+  );
+}
 
 export default Homepage;
