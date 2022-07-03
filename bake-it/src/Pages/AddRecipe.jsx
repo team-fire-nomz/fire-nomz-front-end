a0d9b5a47fb9eea71d1df2e9699af616b096380f
hjoyheath <hillaryjoyheath@gmail.com>
2022-07-03 15:33:32 -0400

    edit and add recipe are submitting, still errors

--- bake-it/src/Pages/AddRecipe.jsx
+++ bake-it/src/Pages/AddRecipe.jsx
@@ -7,13 +7,14 @@ export default function AddRecipe(props) {
   const inputArr = [
     {
       type: "text",
-      id: 1,
       value: "",
     },
   ];
   const [isEntered, setIsEntered] = useState(false);
   const [title, setTitle] = useState("");
-  const [inputs, setInputs] = useState(inputArr);
+  const [ingredientsInputs, setIngredientsInputs] = useState(inputArr);
+  const [recipeInputs, setRecipeInputs] = useState(inputArr);
+
 
   const handleSubmit = (e) => {
     axios
@@ -21,8 +22,8 @@ export default function AddRecipe(props) {
         "https://bake-it-till-you-make-it.herokuapp.com/api/recipes/",
         {
           title: title,
-          ingredients: inputs.map((item) => item.value),
-          recipe_steps: inputs.map((item) => item.value),
+          ingredients: ingredientsInputs.map((item) => item.value),
+          recipe_steps: recipeInputs.map((item) => item.value),
         },
         { headers: { Authorization: `Token ${props.token}` } }
       )
@@ -30,11 +31,15 @@ export default function AddRecipe(props) {
         console.log(res);
         // use react-dom to navigate to homepage
         window.location = "/";
+      })
+      .catch((e) => {
+        console.log(e.response.data);
       });
   };
 
-  const addInput = () => {
-    setInputs((s) => {
+
+  const addIngredientInput = () => {
+    setIngredientsInputs((s) => {
       return [
         ...s,
         {
@@ -45,13 +50,28 @@ export default function AddRecipe(props) {
     });
   };
 
-  const handleInputChange = (e) => {
-    e.preventDefault();
+  const addRecipeInput = () => {
+    setRecipeInputs((s) => {
+      return [
+        ...s,
+        {
+          type: "text",
+          value: "",
+        },
+      ];
+    });
+  };
 
+
+  const handleIngredientInputChange = (e) => {
+    e.preventDefault();
+    console.log(handleIngredientInputChange, "HERE");
     const index = e.target.id;
-    setInputs((s) => {
+    setIngredientsInputs((s) => {
       const newArr = s.slice();
       newArr[index].value = e.target.value;
+      
+
 
       return newArr;
     });
@@ -59,9 +79,10 @@ export default function AddRecipe(props) {
 
   const handleRecipeStepsChange = (e) => {
     e.preventDefault();
+    console.log(handleRecipeStepsChange, "HERE IT IS");
 
     const index = e.target.id;
-    setInputs((s) => {
+    setRecipeInputs((s) => {
       const newArr = s.slice();
       newArr[index].value = e.target.value;
 
@@ -69,20 +90,21 @@ export default function AddRecipe(props) {
     });
   };
 
-  const ingredients = "";
-  inputs.map((item) => ingredients.concat(item.value));
-  console.log(ingredients);
 
-  const recipeSteps = "";
-  inputs.map((item) => recipeSteps.concat(item.value));
-  console.log(recipeSteps);
+  const ingredients = [""];
+  ingredientsInputs.map((item) => ingredients.concat(item.value));
+  console.log(ingredients, "ingredients");
+
+  const recipeSteps = [""];
+  recipeInputs.map((item) => recipeSteps.concat(item.value));
+  console.log(recipeSteps, "recipe");
 
   if (isEntered) {
     return "Your recipe has been submitted.";
   }
 
   return (
-    <Container sx={{ backgroundSize: 'cover', backgroundPosition: 'center', height: '90vh', backgroundImage: `url(${BG1})`, backgroundRepeat: 'no-repeat', overflow: "scroll" }}>
+    <Container sx={{ backgroundPosition: 'center', height: '90vh', backgroundImage: `url(${BG1})`, backgroundRepeat: 'no-repeat', overflow: "scroll" }}>
     <Grid container direction="column" justifyContent="center" alignItems="center">
     <CardContent>
         <label htmlFor="title"/>
@@ -99,26 +121,27 @@ export default function AddRecipe(props) {
     <CardContent>
     <Box textAlign="center">
         <Button 
-        to= "/signup"
         size="small"
         variant="contained"
         type="submit" 
-        onClick={addInput}
+        key="ingredients"
+        onClick={addIngredientInput}
         >
         Add ingredient
         </Button>
     </Box>    
     </CardContent>
     <CardContent>
-        {inputs.map((item, i) => {
+        {ingredientsInputs.map((item, i) => {
             return (
             <input
-                onChange={handleInputChange}
+                onChange={handleIngredientInputChange}
                 placeholder="Ingredient:"
                 value={item.value}
                 id={i}
                 type={item.type}
                 size="40"
+                key={ingredients.toString()}
             />
             );
         })}
@@ -129,14 +152,15 @@ export default function AddRecipe(props) {
         size="small"
         variant="contained"
         type="submit" 
-        onClick={addInput}
+        key="recipeSteps"
+        onClick={addRecipeInput}
         >
         Add recipe steps
         </Button>
     </Box>    
     </CardContent>
     <CardContent>
-        {inputs.map((item, i) => {
+        {recipeInputs.map((item, i) => {
           return (
           <input
               onChange={handleRecipeStepsChange}
@@ -144,6 +168,7 @@ export default function AddRecipe(props) {
               value={item.value}
               id={i}
               type={item.type}
+              key={recipeSteps.toString()}
               size="40"
           />
           );
@@ -165,3 +190,4 @@ export default function AddRecipe(props) {
     </Container>
   );
 }
+
